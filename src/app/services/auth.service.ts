a5e564a0638063c0aa32bc3ca4df3e0af94f4a77
lucas0408 <lucasgabrielnavassabino@gmail.com>
2025-02-26 20:23:01 -0300

    changing routes config

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Login } from '../pages/shared/models/Models';
-import { Observable } from 'rxjs';
+import { ApiResponse, Login } from '../pages/shared/models/Models';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,10 @@ export class AuthService {
   constructor(private httpClient: HttpClient) { }
 
   login(login:Login):Observable<Login> {
-    return this.httpClient.post<Login>(this.apiUrl+"login",login)
+    return this.httpClient.post<Login>(this.apiUrl+"login",login).pipe(
+      catchError(error => {
+        return throwError(() => error.error as ApiResponse<null>);
+      })
+    );;
   }
 }
